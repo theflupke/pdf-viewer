0bea8ddde1760f5f85c251babb7cd86e7bb11bd7
Stéphane Salomon <stephane.salomon@pm.me>
2020-03-23 16:47:25 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$.getJSON("../config.json", function( config ) {
+    document.title = config.pageTitle;
+
+    var source = config.pdf;
+    var options = {
+        "soundEnable": false,
+        "enableDownload": false,
+        "hideControls": "share,more",
+        stiffness: 5,
+    };
+    var flipBook = $(".book").flipBook(source, options);
+});
